b1fbd0fe6f8a1fa5306b129e7d9e9e2e648a3ed7
roDEV0 <gian@romanodev.com>
2026-09-13 22:49:21 -0400

    Basic Robot Setup

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Drivetrain.kt
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.mechanisms
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot
+import com.qualcomm.robotcore.hardware.Gamepad
+import com.qualcomm.robotcore.hardware.IMU
+import dev.nextftc.hardware.actuators.NextMotor
+import dev.nextftc.hardware.sensors.NextIMU
+import dev.nextftc.robot.Mechanism
+import dev.nextftc.robot.drive.mecanumDriveFieldCentric
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
+
+class Drivetrain : Mechanism {
+    val frontLeft = NextMotor("frontLeft")
+    val frontRight = NextMotor("frontRight")
+    val backLeft = NextMotor("backLeft")
+    val backRight = NextMotor("backRight")
+
+    val imu = NextIMU("imu")
+
+    fun imuInit() {
+        imu.initialize(IMU.Parameters(
+            RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
+            )
+        ))
+    }
+
+    fun startDrivetrain(gamepad: Gamepad) {
+        mecanumDriveFieldCentric(
+            frontLeft,
+            frontRight,
+            backLeft,
+            backRight,
+            gamepad,
+            {imu.yawPitchRollAngles.getYaw(AngleUnit.RADIANS)}
+        ).schedule()
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/BaseTele.kt
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop
+
+import dev.nextftc.robot.Telemetry
+import dev.nextftc.robot.opmode.NextOpMode
+import dev.nextftc.robot.opmode.NextTeleop
+import org.firstinspires.ftc.teamcode.BiobuzzBot
+
+@NextTeleop(name = "Base TeleOp")
+class BaseTele(val robot: BiobuzzBot) : NextOpMode(robot) {
+
+    override fun periodic() {
+        Telemetry.log("Currently Running!")
+    }
+
+    override fun start() {
+        robot.drivetrain.startDrivetrain(gamepad1)
+    }
+}
