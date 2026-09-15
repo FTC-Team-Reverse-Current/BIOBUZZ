b90bf039cf40443cd9d34311edd92b4359f19ec5
Timothy <timlarson2011@protonmail.com>
2026-09-14 20:11:49 -0400

    We have added a new message to the periodic function so it says "BioBuzz Bot Running!" instead of being boring.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TestTeleop.kt
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop
+
+import dev.nextftc.robot.Telemetry
+import dev.nextftc.robot.opmode.NextOpMode
+import dev.nextftc.robot.opmode.NextTeleop
+import org.firstinspires.ftc.teamcode.BiobuzzBot
+
+@NextTeleop(name = "Base TeleOp")
+class TestTeleop(
+    val robot: BiobuzzBot,
+) : NextOpMode(robot) {
+    override fun periodic() {
+        Telemetry.log("BioBuzz Bot Running!")
+    }
+
+    override fun start() {
+        robot.drivetrain.startDrivetrain(gamepad1)
+    }
+
+}
