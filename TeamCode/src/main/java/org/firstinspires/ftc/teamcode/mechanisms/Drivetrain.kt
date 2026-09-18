60edb70661f898f11f9079f066fa1d156e2c6a16
roDEV0 <gian@romanodev.com>
2026-09-18 10:22:32 -0400

    Bind to ports instead of config names

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Drivetrain.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Drivetrain.kt
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.mechanisms
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot
 import com.qualcomm.robotcore.hardware.Gamepad
 import com.qualcomm.robotcore.hardware.IMU
+import dev.nextftc.hardware.RobotController
 import dev.nextftc.hardware.actuators.NextMotor
 import dev.nextftc.hardware.sensors.NextIMU
 import dev.nextftc.robot.Mechanism
@@ -10,10 +11,10 @@ import dev.nextftc.robot.drive.mecanumDriveFieldCentric
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
 
 class Drivetrain : Mechanism {
-    val frontLeft = NextMotor("frontLeft")
-    val frontRight = NextMotor("frontRight")
-    val backLeft = NextMotor("backLeft")
-    val backRight = NextMotor("backRight")
+    val frontLeft = NextMotor(RobotController.controlHub, 0)
+    val frontRight = NextMotor(RobotController.controlHub, 1)
+    val backLeft = NextMotor(RobotController.controlHub, 2)
+    val backRight = NextMotor(RobotController.controlHub, 3)
 
     val imu = NextIMU("imu")
 
