1197102ce1f7bbb1979348fdb952695259937005
Michael Kelbick <181020028+michaelkelbick@users.noreply.github.com>
2026-09-20 14:01:29 -0400

    Change gradle files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Drivetrain.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Drivetrain.kt
@@ -3,18 +3,17 @@ package org.firstinspires.ftc.teamcode.mechanisms
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot
 import com.qualcomm.robotcore.hardware.Gamepad
 import com.qualcomm.robotcore.hardware.IMU
-import dev.nextftc.hardware.RobotController
 import dev.nextftc.hardware.actuators.NextMotor
 import dev.nextftc.hardware.sensors.NextIMU
 import dev.nextftc.robot.Mechanism
-import dev.nextftc.robot.drive.mecanumDriveFieldCentric
+import dev.nextftc.robot.drive.mecanumDrive
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
 
 class Drivetrain : Mechanism {
-    val frontLeft = NextMotor(RobotController.controlHub, 0)
-    val frontRight = NextMotor(RobotController.controlHub, 1)
-    val backLeft = NextMotor(RobotController.controlHub, 2)
-    val backRight = NextMotor(RobotController.controlHub, 3)
+    val frontLeft = NextMotor("frontLeft")
+    val frontRight = NextMotor("frontRight")
+    val backLeft = NextMotor("backLeft")
+    val backRight = NextMotor("backRight")
 
     val imu = NextIMU("imu")
 
@@ -23,20 +22,19 @@ class Drivetrain : Mechanism {
             IMU.Parameters(
                 RevHubOrientationOnRobot(
                     RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                    RevHubOrientationOnRobot.UsbFacingDirection.FORWARD,
+                    RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD,
                 ),
             ),
         )
     }
 
     fun startDrivetrain(gamepad: Gamepad) {
-        mecanumDriveFieldCentric(
+        mecanumDrive(
             frontLeft,
             frontRight,
             backLeft,
             backRight,
             gamepad,
-            { imu.yawPitchRollAngles.getYaw(AngleUnit.RADIANS) },
         ).schedule()
     }
 }
